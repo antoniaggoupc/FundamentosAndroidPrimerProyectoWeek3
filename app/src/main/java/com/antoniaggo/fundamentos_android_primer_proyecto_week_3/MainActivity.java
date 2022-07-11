0f72ffcaf95ccf395727b664013415ba331b47a1
Antonia <antonia.gallardo.gomez@gmail.com>
2022-07-11 11:10:43 +0200

    Background

---
+++ app/src/main/java/com/antoniaggo/fundamentos_android_primer_proyecto_week_3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.antoniaggo.fundamentos_android_primer_proyecto_week_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
